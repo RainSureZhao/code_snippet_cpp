f4bf0b2b93b776a49c55940909ee89f2d48f5d34
RainSure <1114560341@qq.com>
2023-04-26 00:26:41 +0800

    add splitmix64 to prevent unorderedmap hacked

---
+++ unorderedmap.cpp
@@ -0,0 +1,10 @@
+struct splitmix64 {
+    size_t operator()(size_t x) const {
+        static const size_t fixed = chrono::steady_clock::now().time_since_epoch().count();
+        x += 0x9e3779b97f4a7c15 + fixed;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+};
+// 用法 unordered_map<int, int, splitmix64> mp;
